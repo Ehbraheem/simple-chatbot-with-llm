614af1ee1f0c0ea75edde2c9dc4eb279f0d10803
Ehbraheem <ehbraheem@gmail.com>
2024-09-13 23:43:22 -0400

    Create backend

---
+++ app.py
@@ -0,0 +1,35 @@
+import json
+
+from flask import Flask, request
+from flask_cors import CORS
+
+from chatbot import respond_to_prompt
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+
+# Request shape
+# {
+#     'prompt': 'message'
+# }
+
+@app.route('/chatbot', methods=['POST'])
+def handle_prompt():
+    data = request.get_data(as_text=True)
+    data = json.loads(data)
+    input_text = data['prompt']
+
+    response = respond_to_prompt(input_text)
+
+    return response
+
+
+
+
+if __name__ == '__main__':
+    app.run()
